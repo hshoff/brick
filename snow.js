77c61406adb122723bed119aa239f2c42d07011b
Harrison Shoff <hi@hshoff.com>
2013-06-14 15:04:41 -0700

    less noise

--- snow.js
+++ snow.js
@@ -4,7 +4,7 @@
       context = canvas.getContext('2d'),
       PI_2 = Math.PI * 2,
       RADIUS = 20,
-      NUM_CONFETTI = 100,
+      NUM_CONFETTI = 50,
       SPEED = 7,
       wCenter,
       hCenter;
